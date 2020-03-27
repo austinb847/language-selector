f79d476bd5e117027a1f13b474a63dbdf3b9096e
austinb847 <austinb847@icloud.com>
2020-03-27 16:06:23 -0700

    add new css styles, and add new image

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,37 @@ $(document).ready(function() {
     var iosSelection = $("input:radio[name=ios]:checked").val();
     var aiSelection = $("input:radio[name=ai]:checked").val();
 
-    console.log(name);
-    console.log(microRating);
-    console.log(cheeseSelection);
-    console.log(iosSelection);
-    console.log(aiSelection);
-
     if (name) {
       if (microRating > 3 && cheeseSelection === "option2") {
         $(".output").html("" +
         "<div class='match'>" +
-        "<h2>" + name + "here is your suggested programming language!</h2>" +
+        "<h2>" + name + " here is your suggested programming language!</h2>" +
         "<p>The Beatutiful C#!</p>" +
         "<img src='img/csharp.jpg'>" +
         "</div>");
       } else if (microRating <= 2 && iosSelection === "yes") {
+        $(".output").html("" +
+        "<div class='match'>" +
+        "<h2>" + name + " here is your suggested programming language!</h2>" +
+        "<p>The Beatutiful Swift!</p>" +
+        "<img src='img/swift.jpg'>" +
+        "</div>");
+ 
+      } else if (aiSelection === "yes" && iosSelection === "yes") {
+        $(".output").html("" +
+        "<div class='match'>" +
+        "<h2>" + name + " here is your suggested programming language!</h2>" +
+        "<p>The Beatutiful Python!</p>" +
+        "<img src='img/python.png'>" +
+        "</div>");
+      } else {
+        $(".output").html("" +
+        "<div class='match'>" +
+        "<h2>" + name + " we couldn't find a match for you... maybe coding isn't for you.</h2>" +
+        "<img src='img/sad.jpg'>" +
+        "</div>");
 
-        
-      }
-
-
-    
+      }   
     } else {
       alert("Enter your name please");
     }
