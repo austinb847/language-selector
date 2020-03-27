b3535177b55bdfd454840c3fc59f23168865b10d
austinb847 <austinb847@icloud.com>
2020-03-27 13:18:52 -0700

    add if/elseblock to determine which language the user is and insert there result via html method

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("form#langMatch").submit(function(event) {
     var name = $("input#name").val();
-    var microRating = $("#microRating").val();
+    var microRating = parseInt($("#microRating").val());
     var cheeseSelection = $("input:radio[name=stringCheese]:checked").val();
     var iosSelection = $("input:radio[name=ios]:checked").val();
     var aiSelection = $("input:radio[name=ai]:checked").val();
@@ -11,6 +11,25 @@ $(document).ready(function() {
     console.log(cheeseSelection);
     console.log(iosSelection);
     console.log(aiSelection);
+
+    if (name) {
+      if (microRating > 3 && cheeseSelection === "option2") {
+        $(".output").html("" +
+        "<div class='match'>" +
+        "<h2>" + name + "here is your suggested programming language!</h2>" +
+        "<p>The Beatutiful C#!</p>" +
+        "<img src='img/csharp.jpg'>" +
+        "</div>");
+      } else if (microRating <= 2 && iosSelection === "yes") {
+
+        
+      }
+
+
+    
+    } else {
+      alert("Enter your name please");
+    }
 
     event.preventDefault();
   });
